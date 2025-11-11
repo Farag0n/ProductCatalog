b3a1edbdd31c2dfb294777283adbe49f2d5f801b
Farag0n <miguelanga1604@gmail.com>
2025-11-11 10:58:29 -0500

    little fix

--- ProductCatalog.Infraestructure/obj/Debug/net8.0/ProductCatalog.Infraestructure.AssemblyInfo.cs
+++ ProductCatalog.Infraestructure/obj/Debug/net8.0/ProductCatalog.Infraestructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductCatalog.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a975b99bddec9b666b989ed485ecb12dce79950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d676a66537742a4c91b19b50299b646c5a8e5bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductCatalog.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductCatalog.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
